dddbefe9ca5f68efa323c0312c711e31280b40c1
Yillia <tobia.schaerli@edu.teko.ch>
2025-02-18 13:21:53 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,62 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'SUP';
+  prognoseAnzahl = '';
+  rentalForm: FormGroup = new FormGroup([]);
+  customerList: string[] = ['Max Musterman', 'Susie Muster'];
+  supList: string[] = ['1er', '2er'];
+  newCustomer = false;
+
+  constructor(private fb: FormBuilder) {
+    this.rentalForm = this.fb.group({
+      customer: [''],
+      sup: [''],
+      startTime: [''],
+      endTime: [''],
+      street: [''],
+      plz: [''],
+      city: [''],
+      securityInformed: [false],
+      luggage: [false],
+      luggageCount: [0],
+    });
+  }
+
+  onSubmit() {
+    console.log('Buchung:', this.rentalForm.value);
+  }
+
+  getPrognose() {
+    const anzahl = Math.round(Math.random() * (10 - 0) + 0);
+    this.prognoseAnzahl = anzahl + ' SUP können heute vermietet werden.';
+    if (anzahl === 0) {
+      this.rentalForm.disable();
+    } else {
+      this.rentalForm.enable();
+    }
+  }
+
+  setSUPType(event: any) {
+    if (event === true) {
+      this.rentalForm.get('customer')?.setValue('');
+      this.rentalForm.get('sup')?.setValue('1er');
+      this.rentalForm.get('sup')?.disable();
+    } else {
+      this.rentalForm.get('sup')?.enable();
+    }
+  }
 }
